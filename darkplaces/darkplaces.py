d148893a2d34513febf50cefd3a2e21963e98d88
Steffen Møgelmose <62184112+smogelmose@users.noreply.github.com>
2021-09-02 14:13:03 +0200

    updated darkplaces.py with code

--- darkplaces/darkplaces.py
+++ darkplaces/darkplaces.py
@@ -1,1 +1,25 @@
+//darkplaces
 
+import rhinoscriptsyntax as rs
+import random
+import math
+
+z =-100
+radius = 0
+theta = 0
+
+count = 0
+while z <= 100:
+	if count % 2 ==0:
+		x = (radius + 2) * math.cos(theta)
+		y = (radius + 2) * math.sin(theta)
+	else:
+		x = radius * math.cos(theta)
+		y = radius * math.sin(theta)
+	
+	rs.AddPoint((x,y,z))
+
+	radius += .005
+	theta += 2
+	z = z + .05
+	count += 1
